6f53db7ce368f97ceff6e325a62bfc7331f9f4e8
mrjunoh <mrjunoh@naver.com>
2023-06-07 14:55:46 +0900

    이미지_출력_추가
    이미지_출력_추가

--- src/Pages/Chat5.js
+++ src/Pages/Chat5.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import {v1 as uuid} from 'uuid'
 import axios from 'axios';
 import './css/sty.css';
 
 const Chat5 = () => {
-  const [myName] = useState('blue');
+  const [myName] = useState('USER');
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [botResponse, setBotResponse] = useState('');
@@ -21,36 +22,34 @@ const Chat5 = () => {
     fetchMessages();
   }, []);
 
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 13 && !e.shiftKey) {
-      e.preventDefault();
-      const message = e.target.value;
-      sendMessage(message);
-      clearTextarea();
-    }
-  };
 
-  const createMessageTag = (LR_className, senderName, message) => {
+
+  const createMessageTag = (LR_className, senderName, message, imageUrl) => {
     return (
-      <li className={LR_className}>
+      <li key={uuid()} className={LR_className}>
         <div className="sender">
           <span>{senderName}</span>
         </div>
         <div className="message">
           <span>{message}</span>
         </div>
+        {
+          imageUrl && imageUrl !=='nan' && imageUrl !== '없음' && <div>
+            <img src={imageUrl} />
+          </div>
+        }
       </li>
     );
   };
 
-  const appendMessageTag = (LR_className, senderName, message) => {
-    const chatLi = createMessageTag(LR_className, senderName, message);
+  const appendMessageTag = (LR_className, senderName, message, imageUrl) => {
+    const chatLi = createMessageTag(LR_className, senderName, message, imageUrl);
     setMessages(prevMessages => [...prevMessages, chatLi]);
   };
 
   const sendMessage = (message) => {
     const data = {
-      senderName: 'blue',
+      senderName: 'USER',
       message: message,
     };
     receive(data);
@@ -62,23 +61,50 @@ const Chat5 = () => {
 
   const receive = (data) => {
     const LR = data.senderName !== myName ? 'left' : 'right';
-    appendMessageTag(LR, data.senderName, data.message);
+    appendMessageTag(LR, data.senderName, data.message, data.imageUrl);
   };
 
-  const handleSubmit = async e => {
-    e.preventDefault();
+  const triggerGhatGpt = async (message) => {
+    sendMessage(message);
+    setInputValue('')
     try {
       const res = await axios.post(
         'http://localhost:8000/query/NORMAL',
         { query: inputValue }
       );
-      setMessages(prevMessages => [...prevMessages, res.data.Answer]);
-      setInputValue('');
+      // setMessages(prevMessages => [...prevMessages, res.data.Answer]);
+      appendMessageTag("", "BOT", res.data.Answer, res.data.imageUrl)
       console.log(res);
       setBotResponse(res.data.response);
     } catch (err) {
       console.error(err);
     }
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13 && !e.shiftKey) {
+      e.preventDefault();
+      // const message = e.target.value;
+      // sendMessage(message);
+      triggerGhatGpt(inputValue)
+    }
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    // try {
+    //   const res = await axios.post(
+    //     'http://localhost:8000/query/NORMAL',
+    //     { query: inputValue }
+    //   );
+    //   setMessages(prevMessages => [...prevMessages, res.data.Answer]);
+    //   setInputValue('');
+    //   console.log(res);
+    //   setBotResponse(res.data.response);
+    // } catch (err) {
+    //   console.error(err);
+    // }
+    triggerGhatGpt(inputValue);
   };
 
   const handleChange = e => {
@@ -98,7 +124,7 @@ const Chat5 = () => {
           onChange={handleChange}
           value={inputValue}
         ></textarea>
-        <button onClick={handleSubmit}>Send</button>
+        {/* <button onClick={handleSubmit}>Send</button> */}
       </div>
     </div>
   );
